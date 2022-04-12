a667e234d510384c50e72d6507f5907420bade80
Prejin Senan <93463606+prejin2310@users.noreply.github.com>
2022-04-12 22:24:58 +0530

    error fixed - cannot access Product1

--- CO1/1.Product-Class/product.java
+++ CO1/1.Product-Class/product.java
@@ -1,30 +1,46 @@
-class product
+class product1
 {
-  String pcode,pname;
-  int price;
- 
-  public product(String code, String name, int rs)
-  {
-    pcode=code;
-    pname=name;
-    price=rs;
-  }
-  public double Dis()
-  {
-    return price;
-  }
+    int pcode;
+    String pname;
+    double price;
+    void data(int c, String n , double p)
+      {
+        pcode = c;
+        pname = n;
+        price = p;
+      }
+    void display()
+      {
+        System.out.println(pcode+"\t\t"+pname+"\t\t"+price);
+      }
+    static void findLowest(double price1 , double price2 , double price3)
+     {
+        if(price1<=price2 && price1<=price3)
+        {
+            System.out.println("\nProduct 1 is of the lowest price!");
+        }
+        else if(price2 <= price1 && price2 <= price3)
+        {
+            System.out.println("\nProduct2 is of the lowest price!");
+        }
+        else
+        {
+            System.out.println("\nProduct 3 is of the lowest price!");
+        }
+     }
 
-public static void main(String [] args)
- {
-  product p1=new product("A100","Prejin",5000);
-  product p2=new product("A102","Renjith",3000);
-  product p3=new product("A104","Anu",6000);
-
- if(p1.Dis()>=p2.Dis() && p1.Dis()>=p3.Dis())
-  System.out.println(p1.pname+" Salary is high");
- if(p2.Dis()>=p3.Dis() && p2.Dis()>=p1.Dis())
-  System.out.println(p2.pname+" Salary is high");
- if(p3.Dis()>=p2.Dis() && p3.Dis()>=p1.Dis())
-  System.out.println(p3.pname+" Salary is high");
-}
+public static void main(String[] args)
+  {
+    product1 obj1 = new product1();
+    product1 obj2 = new product1();
+    product1 obj3 = new product1();
+    obj1.data(101, "product_1",100.0);
+    obj2.data(102, "product_2",128.40);
+    obj3.data(103, "product_3",790.00);
+    System.out.println("Product Information:\n\n Product_code\tProduct_Name\tProduct_price");
+    obj1.display();
+    obj2.display();
+    obj3.display();
+    findLowest(obj1.price,obj2.price,obj3.price);
+  }
 }
